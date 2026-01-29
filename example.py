01d12f5826c172fb858fb83e1c4d67fc99512350
rajithan-2002 <rajipathman420@gmail.com>
2026-01-29 13:52:20 +0530

    Added new addition file to the project

---
+++ example.py
@@ -0,0 +1,5 @@
+# Simple program to calculate the sum of two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_result = num1 + num2
+print("The sum is:", sum_result)
